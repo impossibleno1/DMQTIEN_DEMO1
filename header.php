f9d8f8b2103ed2e8a86d50cf6a3ea79e9e522c01
Thanh Dang <dangbmt1998@gmail.com>
2018-11-28 17:11:07 +0700

    set header footer

---
+++ header.php
@@ -0,0 +1,288 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Free Smart Store Website Template | Home :: w3layouts</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<link href="css/menu.css" rel="stylesheet" type="text/css" media="all"/>
+<script src="js/jquery.min.js"></script>
+<script src="js/script.js" type="text/javascript"></script>
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<link href='//fonts.googleapis.com/css?family=Monda' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Doppio+One' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <div class="wrap">
+	<div class="header">
+		<div class="header_top">
+			<div class="logo">
+				<a href="index.php"><img src="images/logo.png" alt="" /></a>
+			</div>
+			  <div class="header_top_right">
+			    <div class="search_box">
+				    <form>
+				    	<input type="text" value="Search for Products" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search for Products';}"><input type="submit" value="SEARCH">
+				    </form>
+			    </div>
+			    <div class="shopping_cart">
+					<div class="cart">
+						<a href="#" title="View my shopping cart" rel="nofollow">
+							<strong class="opencart"> </strong>
+								<span class="cart_title">Cart</span>
+									<span class="no_product">(empty)</span>
+							</a>
+						</div>
+			      </div>
+	    <div class="languages" title="language">
+	    	<div id="language" class="wrapper-dropdown" tabindex="1">EN
+						<strong class="opencart"> </strong>
+						<ul class="dropdown languges">					
+							 <li>
+							 	<a href="#" title="Français">
+									<span><img src="images/gb.png" alt="en" width="26" height="26"></span><span class="lang">English</span>
+								</a>
+							 </li>
+								<li>
+									<a href="#" title="Français">
+										<span><img src="images/au.png" alt="fr" width="26" height="26"></span><span class="lang">Français</span>
+									</a>
+								</li>
+						<li>
+							<a href="#" title="Español">
+								<span><img src="images/bm.png" alt="es" width="26" height="26"></span><span class="lang">Español</span>
+							</a>
+						</li>
+								<li>
+									<a href="#" title="Deutsch">
+										<span><img src="images/ck.png" alt="de" width="26" height="26"></span><span class="lang">Deutsch</span>
+									</a>
+								</li>
+						<li>
+							<a href="#" title="Russian">
+								<span><img src="images/cu.png" alt="ru" width="26" height="26"></span><span class="lang">Russian</span>
+							</a>
+						</li>					
+				   </ul>
+		     </div>
+		     <script type="text/javascript">
+			function DropDown(el) {
+				this.dd = el;
+				this.initEvents();
+			}
+			DropDown.prototype = {
+				initEvents : function() {
+					var obj = this;
+
+					obj.dd.on('click', function(event){
+						$(this).toggleClass('active');
+						event.stopPropagation();
+					});	
+				}
+			}
+
+			$(function() {
+
+				var dd = new DropDown( $('#language') );
+
+				$(document).click(function() {
+					// all dropdowns
+					$('.wrapper-dropdown').removeClass('active');
+				});
+
+			});
+
+		</script>
+		 </div>
+			<div class="currency" title="currency">
+					<div id="currency" class="wrapper-dropdown" tabindex="1">$
+						<strong class="opencart"> </strong>
+						<ul class="dropdown">
+							<li><a href="#"><span>$</span>Dollar</a></li>
+							<li><a href="#"><span>€</span>Euro</a></li>
+						</ul>
+					</div>
+					 <script type="text/javascript">
+			function DropDown(el) {
+				this.dd = el;
+				this.initEvents();
+			}
+			DropDown.prototype = {
+				initEvents : function() {
+					var obj = this;
+
+					obj.dd.on('click', function(event){
+						$(this).toggleClass('active');
+						event.stopPropagation();
+					});	
+				}
+			}
+
+			$(function() {
+
+				var dd = new DropDown( $('#currency') );
+
+				$(document).click(function() {
+					// all dropdowns
+					$('.wrapper-dropdown').removeClass('active');
+				});
+
+			});
+
+		</script>
+   </div>
+		   <div class="login">
+		   	   <span><a href="login.php"><img src="images/login.png" alt="" title="login"/></a></span>
+		   </div>
+		 <div class="clear"></div>
+	 </div>
+	 <div class="clear"></div>
+ </div>
+	<div class="h_menu">
+		<a id="touch-menu" class="mobile-menu" href="#">Menu</a>
+		<nav>
+		<ul class="menu list-unstyled">
+			<li><a href="index.php">HOME</a></li>
+			<li class="activate"><a href="products.php">Products</a>
+				<ul class="sub-menu list-unstyled">
+				 <div class="nag-mother-list">
+				   <div class="navg-drop-main">
+					<div class="nav-drop"> 
+						<li><a href="products.php">Product 1</a></li>
+						<li><a href="products.php">Product 2</a></li>
+						<li><a href="products.php">Product 3</a>
+						</li>
+					</div>
+					<div class="nav-drop"> 
+						<li><a href="products.php">Product 1</a></li>
+						<li><a href="products.php">Product 2</a></li>
+						<li><a href="products.php">Product 3</a>
+								</li>
+					</div>
+					<div class="nav-drop"> 
+						<li><a href="products.php">Product 1</a></li>
+						<li><a href="products.php">Product 2</a></li>
+						<li><a href="products.php">Product 3</a></li>
+					</div>
+					<div class="nav-drop"> 
+						<li><a href="products.php">Product 1</a></li>
+						<li><a href="products.php">Product 2</a></li>
+						<li><a href="products.php">Product 3</a></li>
+					</div>
+					<div class="clear"> </div>
+				</div>
+				<div class="navg-drop-main">
+					<div class="nav-drop"> 
+						<li><a href="products.php">Product 4</a></li>
+						<li><a href="products.php">Product 5</a></li>
+						<li><a href="products.php">Product 6</a>
+		
+						</li>
+					</div>
+					<div class="nav-drop"> 
+						<li><a href="products.php">Product 4</a></li>
+						<li><a href="products.php">Product 5</a></li>
+						<li><a href="products.php">Product 6</a>
+								</li>
+					</div>
+					<div class="nav-drop"> 
+						<li><a href="products.php">Product 4</a></li>
+						<li><a href="products.php">Product 5</a></li>
+						<li><a href="products.php">Product 6</a></li>
+					</div>
+					<div class="nav-drop"> 
+						<li><a href="products.php">Product 4</a></li>
+						<li><a href="products.php">Product 5</a></li>
+						<li><a href="products.php">Product 6</a></li>
+					</div>
+					<div class="clear"> </div>
+				 </div>
+				 <div class="navg-drop-main">
+					<div class="nav-drop"> 
+						<li><a href="products.php">Product 4</a></li>
+						<li><a href="products.php">Product 5</a></li>
+						<li><a href="products.php">Product 6</a>
+		
+						</li>
+					</div>
+					<div class="nav-drop"> 
+						<li><a href="products.php">Product 4</a></li>
+						<li><a href="products.php">Product 5</a></li>
+						<li><a href="products.php">Product 6</a>
+								</li>
+					</div>
+					<div class="nav-drop"> 
+						<li><a href="products.php">Product 4</a></li>
+						<li><a href="products.php">Product 5</a></li>
+						<li><a href="products.php">Product 6</a></li>
+					</div>
+					<div class="nav-drop"> 
+						<li><a href="products.php">Product 4</a></li>
+						<li><a href="products.php">Product 5</a></li>
+						<li><a href="products.php">Product 6</a></li>
+					</div>
+					<div class="clear"> </div>
+				 </div>
+				</div>
+			</ul>
+			</li>
+			<li><a href="products.php">Top Brand</a>			
+				<ul class="sub-menu list-unstyled sub-menu2">
+				  <div class="navg-drop-main">
+						<div class="nav-drop nav-top-brand"> 
+							<li><a href="products.php">Product 1</a></li>
+							<li><a href="products.php">Product 2</a></li>
+							<li><a href="products.php">Product 3</a></li>					
+							<li><a href="products.php">Product 4</a></li>
+						   <li><a href="products.php">Product 5</a></li>
+						   <li><a href="products.php">Product 6</a></li>
+						</div>								
+					</div>
+				</ul>
+			</li>		
+			<li><a href="faq.php">Services</a>
+				<ul class="sub-menu list-unstyled sub-menu3">
+				  <div class="navg-drop-main">
+					<div class="nav-drop"> 
+						<li><a href="products.php">Product 4</a></li>
+						<li><a href="products.php">Product 5</a></li>
+						<li><a href="products.php">Product 6</a>
+		
+						</li>
+					</div>
+					<div class="nav-drop"> 
+						<li><a href="products.php">Product 4</a></li>
+						<li><a href="products.php">Product 5</a></li>
+						<li><a href="products.php">Product 6</a>
+								</li>
+					</div>
+					<div class="nav-drop"> 
+						<li><a href="products.php">Product 4</a></li>
+						<li><a href="products.php">Product 5</a></li>
+						<li><a href="products.php">Product 6</a></li>
+					</div>
+					<div class="nav-drop"> 
+						<li><a href="products.php">Product 4</a></li>
+						<li><a href="products.php">Product 5</a></li>
+						<li><a href="products.php">Product 6</a></li>
+					</div>
+					<div class="clear"> </div>
+				 </div>
+			  </ul>
+			</li>
+			<li><a href="about.php">About</a></li>
+			<li><a href="#">Delivery</a></li>
+			<li><a href="faq.php">Faqs</a></li>
+			<li><a href="contact.php">CONTACT</a></li>
+			<div class="clear"> </div>
+		</ul>
+		</nav> 
+		<script src="js/menu.js" type="text/javascript"></script>
+		</div>
