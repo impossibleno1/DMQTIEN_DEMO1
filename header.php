aeff1acb65e0c198d9a8772f420fd9348ad25611
Dat <qdat19051998@gmail.com>
2018-11-28 19:55:10 +0700

    about

--- header.php
+++ header.php
@@ -7,6 +7,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <!DOCTYPE HTML>
 <html>
 <head>
+<meta charset="UTF-8">	
 <title>Free Smart Store Website Template | Home :: w3layouts</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
